8e1e82ae0d1b34f62a2aa422a2b49015ac12109f
rhlkmr <mr.rahulkumar.iit.kgp@gmail.com>
2024-06-14 20:54:00 +0530

    slider image added

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,32 +1,33 @@
 import './Carousel.scss';
+import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-// import OwlCarousel from 'react-owl-carousel2';
+
 
 
 export const Carousel = () => {
     const options = {
-        items: 1,
+        loop: true,
+        margin: 10,
         nav: true,
-        rewind: true,
-        autoplay: true
+        responsive: {
+            1000: {
+                items: 1
+            }
+        }
     };
     return (
-        <div id='wrapper'>
-            <div className="owl-carousel" id='slider-area'>
+        <OwlCarousel className="owl-theme" {...options}>
+            <div className="item">
                 <img src="assets/slider01.avif" alt="" />
+                <h4>1</h4></div>
+            <div className="item">
                 <img src="assets/slider02.avif" alt="" />
+                <h4>2</h4></div>
+            <div className="item">
                 <img src="assets/slider3.avif" alt="" />
-            </div>
-            <div className="slider-text">
-                <h1>Make your Online Payment
-                    <br />
-                    Safer &amp; Comfortable</h1>
-                <p>Offer payment methods flexibility. Customers prefer a variety of payment options at the checkout.</p>
-                <a class="common-btn" href="/">Let's Start Now
-                    <span></span>
-                </a>
-            </div>
-        </div>
+
+                <h4>3</h4></div>
+        </OwlCarousel>
     )
 }
